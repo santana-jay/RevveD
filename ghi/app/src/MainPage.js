34067da2e234ea7bca17f85dfbd9383d36fb8a18
junhanan@gmail.com <junhanan@gmail.com>
2023-06-09 09:52:42 -0400

    final branch push

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,6 +1,11 @@
+import React from "react"
+import frontPagePicture from "./images/frontpage-picture.jpg";
+
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
+      <img src={frontPagePicture} alt="Front Page" />
       <h1 className="display-5 fw-bold">CarCar</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
